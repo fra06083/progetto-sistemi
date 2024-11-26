5f1f0c6a4e566f62adad2abecfc46e9c52cdc6a1
Francesco Biglieri <francescobiglie03@gmail.com>
2024-11-26 13:10:35 +0100

    build

---
+++ build/CMakeFiles/MultiPandOS.dir/phase1/p1test.c.o.d
@@ -0,0 +1,13 @@
+CMakeFiles/MultiPandOS.dir/phase1/p1test.c.o: \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/p1test.c \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/../headers/const.h \
+ /usr/include/uriscv/const.h /usr/include/uriscv/csr.h \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/../headers/types.h \
+ /usr/include/uriscv/types.h \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/../headers/./const.h \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/../headers/./listx.h \
+ /usr/include/uriscv/liburiscv.h \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/./headers/pcb.h \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/./headers/../../headers/listx.h \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/./headers/../../headers/types.h \
+ /home/francesco/Documents/progetto/progetto-sistemi/phase1/./headers/asl.h
