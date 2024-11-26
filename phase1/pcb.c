62c743335777348b65895ac724b05414d1d8227a
Francesco Biglieri <francescobiglie03@gmail.com>
2024-11-26 15:27:25 +0100

    commenti cosa fanno

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -1,13 +1,23 @@
 #include "./headers/pcb.h"
 
-static struct list_head pcbFree_h;
-static pcb_t pcbFree_table[MAXPROC];
-static int next_pid = 1;
+static struct list_head pcbFree_h; // sentinella della lista dei processi liberi o non utilizzati, NON NE FA PARTE.
+static pcb_t pcbFree_table[MAXPROC]; // tabella processi
+static int next_pid = 1; // prossimo id del processo
 
+/*
+
+
+
+*/
 void initPcbs() {
+ INIT_LIST_HEAD(&pcbFree_h);
+for (int i = 0; i<MAXPROC; i++){
+}
 }
 
-void freePcb(pcb_t* p) {
+void freePcb(pcb_t* p) { // inserire l'elemento p nella lista pcbfree ()
+   // pcb_PTR lista = 
+   // for (int i = 0)
 }
 
 pcb_t* allocPcb() {
