d74acf5b27bbb2b2e5ecc5194f4827926e41f633
db4-git <davidebdv2004@gmail.com>
2025-06-09 14:37:36 +0200

    moduli phase3 prova

--- headers/const.h
+++ headers/const.h
@@ -50,7 +50,7 @@
 #define PASSEREN      -3
 #define VERHOGEN      -4
 #define DOIO          -5
-#define GETTIME       -6 // si chiama getcpu nel file
+#define GETTIME       -6
 #define CLOCKWAIT     -7
 #define GETSUPPORTPTR -8
 #define GETPROCESSID  -9
@@ -241,4 +241,3 @@
 /* Task Priority Register */
 #define TPR 0x10000408 
 #endif
-
