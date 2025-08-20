985467e027c133bf0bbb9c529b661781b5ccc4d7
db4-git <davidebdv2004@gmail.com>
2025-08-20 20:28:54 +0200

    Syscall Support Level imbastite, da rivedere (e verificare) alcune cose

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -11,24 +11,58 @@ void SYS2() {
 }
 
 void SYS3(state_t *stato){
-   char *VAddr_first_char = (char *) stato->reg_a1; 
-      int str_len = stato->reg_a2;
-      //Controllo che l'indirizzo virtuale del primo char sia entro il logical U-Proc Address Space, non ecceda la lunghezza del  e che la lunghezza della stringa sia accettabile
-      //In teoria nel logical U-Proc Address Space è limitato da sotto da UPROCSTARTADDR e sopra da USERSTACKTOP 
-      if((str_len < 0 || str_len > 128) || VAddr_first_char < (char *) UPROCSTARTADDR || VAddr_first_char >= (char *) USERSTACKTOP) {
-          SYS2();
-          return; 
-      }
-      int retStatus = SYSCALL(WRITEPRINTER, (unsigned int) VAddr_first_char, str_len, 0);
-      //Devo vedere che tipo di errore mi ritorna, se è diverso da 1 allora c'è stato un errore 
-      if(retStatus != 1) {
-          stato->reg_a0 = -retStatus;
-      }
-      stato->pc_epc += 4; // incrementa il program counter
-      LDST(stato); // ripristina lo stato del processo corrente 
+    char* VAddr_first_char = (char *) stato->reg_a1; 
+    int str_len = stato->reg_a2;
+    //Controllo che l'indirizzo virtuale del primo char sia entro il logical U-Proc Address Space, non ecceda la lunghezza del  e che la lunghezza della stringa sia accettabile
+    //In teoria nel logical U-Proc Address Space è limitato da sotto da UPROCSTARTADDR e sopra da USERSTACKTOP 
+    if((str_len < 0 || str_len > 128) || VAddr_first_char < (char *) UPROCSTARTADDR || VAddr_first_char >= (char *) USERSTACKTOP) {
+        SYS2();
+        return; 
+    }
+    int retStatus = SYSCALL(WRITEPRINTER, &VAddr_first_char, str_len, 0);
+    //Devo vedere che tipo di errore mi ritorna, se è diverso da 1 allora c'è stato un errore 
+    if(retStatus != 1) {
+        stato->reg_a0 = -retStatus;
+    }
+    //Se l'operazione ha avuto successo, in a0 c'è il numero di caratteri trasmessi 
+    stato->reg_a0 = str_len;
+    stato->pc_epc += 4; // incrementa il program counter
+    LDST(stato); // ripristina lo stato del processo corrente (va messo in teoria anche se non scritto perchè il processo viene sospeso)
+}
+
+void SYS4(state_t *stato){
+    char *VAddr_first_char = (char *) stato->reg_a1; 
+    int str_len = stato->reg_a2;
+    if((str_len < 0 || str_len > 128) || VAddr_first_char < (char *) UPROCSTARTADDR || VAddr_first_char >= (char *) USERSTACKTOP) {
+        SYS2();
+        return; 
+    }
+    int retStatus = SYSCALL(WRITETERMINAL, &VAddr_first_char, str_len, 0);
+    if(retStatus != 5){
+        stato->reg_a0 = -retStatus; 
+    }
+    //Se l'operazione ha avuto successo, in a0 c'è il numero di caratteri trasmessi 
+    stato->reg_a0 = str_len;
+    stato->pc_epc += 4; 
+    LDST(stato);
 }
 
 
+void SYS5(state_t* stato){
+    char *Vaddr_start_buff = (char *) stato->reg_a1; 
+    int retStatus = SYSCALL(READTERMINAL, &Vaddr_start_buff, 0, 0);
+    if(Vaddr_start_buff < (char *) UPROCSTARTADDR || Vaddr_start_buff >= (char *) USERSTACKTOP) {
+        SYS2();
+        return; 
+    }
+    int retStatus = SYSCALL(READTERMINAL, &Vaddr_start_buff, 0, 0); 
+    if(retStatus != 5){
+        stato->reg_a0 = -retStatus; 
+    }
+    //Qua dice che in a0 deve esserci il numero dei caratteri della stringa, qua come si ricava? 
+    stato->pc_epc += 4; 
+    LDST(stato);
+}
 
 
 
@@ -58,11 +92,11 @@ void generalExceptionHandler(){
             break;
 
         case WRITETERMINAL: // SYS4
-            // TODO: WRITE TO TERMINAL
+            SYS4(state);
             break;
 
         case READTERMINAL:  // SYS5
-            // TODO: READ FROM TERMINAL
+            SYS5(state); 
             break;
 
         default:
